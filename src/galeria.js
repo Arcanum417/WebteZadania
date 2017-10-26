9926f566c5b4af4d27d63b64da47212637370aa4
Arcanum417 <lubos.len@gmail.com>
2017-10-26 20:55:56 +0200

    Zad4

---
+++ src/galeria.js
@@ -0,0 +1,65 @@
+var jsonData;
+var sIndex = 1;
+var casovac;
+
+function load() {
+    jsonData = JSON.parse(p);
+    document.getElementById('obr1').src = jsonData.photos[0].src;
+    document.getElementById('obr2').src = jsonData.photos[1].src;
+    document.getElementById('obr3').src = jsonData.photos[2].src;
+    document.getElementById('obr4').src = jsonData.photos[3].src;
+    document.getElementById('obr5').src = jsonData.photos[4].src;
+    document.getElementById('obr11').src = jsonData.photos[0].src;
+    document.getElementById('obr12').src = jsonData.photos[1].src;
+    document.getElementById('obr13').src = jsonData.photos[2].src;
+    document.getElementById('obr14').src = jsonData.photos[3].src;
+    document.getElementById('obr15').src = jsonData.photos[4].src;
+}
+
+function openLightBox() {
+    document.getElementById('lightboxVrstva').style.display = "block";
+    document.getElementById('podklad').style.display = "block";
+}
+
+function closeLightBox() {
+    document.getElementById('lightboxVrstva').style.display = "none";
+    document.getElementById('podklad').style.display = "none";
+    prezentaciaStop();
+}
+
+function dalsiSlide(n) {
+    Slide(sIndex += n);
+}
+
+function tentoSlide(n) {
+    Slide(sIndex = n);
+}
+
+function prezentacia() {
+    casovac = window.setInterval(function() {
+        dalsiSlide(1);
+    }, 4500);
+}
+
+function prezentaciaStop() {
+    window.clearInterval(casovac);
+}
+
+function Slide(n) {
+    var i;
+    var slidy = document.getElementsByClassName("slidy");
+    var captionText = document.getElementById("popis");
+    var titleText = document.getElementById("title");
+
+    if (n > slidy.length) { sIndex = 1 }
+
+    if (n < 1) { sIndex = slidy.length }
+
+    for (i = 0; i < slidy.length; i++) {
+        slidy[i].style.display = "none";
+    }
+
+    slidy[sIndex - 1].style.display = "block";
+    captionText.innerHTML = jsonData.photos[sIndex - 1].description;
+    titleText.innerHTML = jsonData.photos[sIndex - 1].title;
+}
---
+++ src/photos.js
@@ -0,0 +1,29 @@
+var p = `{
+  "photos": [
+      {
+        "src": "images/img1.jpg",
+        "title": "Orol",
+        "description": "No ta orol to je co ti poviem"
+      },
+      {
+        "src": "images/img2.jpg",
+        "title": "Gaštany",
+        "description": "Možno už vykupujú"
+      },
+      {
+        "src": "images/img3.jpg",
+        "title": "Lopta",
+        "description": "Ta je na trave"
+      },
+      {
+        "src": "images/img4.jpg",
+        "title": "Bodliak",
+        "description": "Alebo ini kvitok, ja neviem uz"
+      },
+      {
+        "src": "images/img5.jpg",
+        "title": "Ohen",
+        "description": "Buuuuuurn"
+      }      
+    ]
+}`
