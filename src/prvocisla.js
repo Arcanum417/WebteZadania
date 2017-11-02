17ac4745c51e4ee224e4654e988cf5dd4656d4d0
Arcanum417 <lubos.len@gmail.com>
2017-11-02 23:15:36 +0100

    fix

--- src/prvocisla.js
+++ src/prvocisla.js
@@ -7,9 +7,9 @@
         worker.addEventListener('message', function(e) {
             document.getElementById("prime-numbers").textContent += e.data.prime +", ";
             var max = d.querySelector('#limit-high').value
-            document.getElementById("progress").innerHTML = Math.round(e.data.prime/max*100) + "%";
+            document.getElementById("progress").innerHTML = "Progress:" + Math.round(e.data.prime/max*100) + "%";
             if (Math.round(e.data.prime/max*100)> 96){
-                document.getElementById("progress").innerHTML = "100%";
+                document.getElementById("progress").innerHTML = "Progress: 100%";
             }
         });
 
